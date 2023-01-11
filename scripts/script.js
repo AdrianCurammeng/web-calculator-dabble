e3cdd40aa991b1cf16630a88d42dd0f75eacd0f6
Adrian Curammeng <adrianvcurammeng@gmail.com>
2023-01-11 11:17:07 -0500

    Added variables for pad

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,34 @@
+//Variables for Numbers
+const zero = document.querySelector(".zero");
 const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+const six = document.querySelector(".six");
+const seven = document.querySelector(".seven");
+const eight = document.querySelector(".eight");
+const nine = document.querySelector(".nine");
 
-one.addEventListener("click", test);
+const decimal = document.querySelector(".decimal");
+const equals = document.querySelector(".equals");
 
-function test() {
-    console.log("Success");
-}
+const add = document.querySelector(".add");
+const substract = document.querySelector(".subtract");
+const multiply = document.querySelector(".multiply");
+const divide = document.querySelector(".divide");
+const percent = document.querySelector(".percent");
+const squared = document.querySelector(".squared");
+const clear = document.querySelector(".clear");
+const backspace = document.querySelector(".backspace");
+
+const screen = document.querySelector(".screen");
+
+//Button Functions
+
+// [document.querySelector('.a-class'), document.querySelector('.another-class')].forEach(item => {
+//     item.addEventListener('click', event => {
+//       //handle click
+//     })
+//   })
 
